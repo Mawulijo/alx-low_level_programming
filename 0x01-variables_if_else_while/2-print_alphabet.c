eb20e5f176109ada3d79ffac719d417a3674dd29
Mawuli Agbeku <mawulidickson@gmail.com>
2022-12-12 14:44:21 -0800

    prints the alphabet in lowercase, and then in uppercase, followed by a new line

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-char letter;
+	char letter;
 
-letter = 'a';
-while (letter <= 'z')
-{
-putchar(letter);
-letter++;
-}
-putchar('\n');
-return (0);
+	letter = 'a';
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
 }
