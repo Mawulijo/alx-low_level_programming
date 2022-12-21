231fc277aba7916ca8d2ccb3c787a13d06b81491
Joshua Agbeku <mawulidickson@gmail.com>
2022-12-21 15:24:41 +0000

    A dream doesn't become reality through magic

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_number - prints integer value
+ * @n: input integer
+ *
+ */
+
+void print_number(int n)
+{
+	int i = 0;
+	int num = n;
+	int e = 0;
+	int divider = 0;
+	int counter = 0;
+
+	while (num / 10 != 0)
+	{
+		num = num / 10;
+		counter++;
+	}
+	for (e = counter; e >= 0; e--)
+	{
+		divider = 1;
+		for (i = 0; i < e; i++)
+		{
+			divider = divider * 10;
+		}
+		if (n < 0)
+		{
+			_putchar('-');
+			_putchar((0 - (n / divider)) + '0');
+			n = n % divider;
+			n = 0 - n;
+		}
+		else
+		{
+			_putchar((n / divider) + '0');
+			n = n % divider;
+		}
+	}
+}
