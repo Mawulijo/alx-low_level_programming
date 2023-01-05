05ef1fe8ce347f645fd6da0d710f85df630bb59f
Joshua Agbeku <mawulidickson@gmail.com>
2023-01-05 14:41:39 +0000

    a function that prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,13 +9,10 @@
 void _puts_recursion(char *s)
 {
 	if (s[0] == '\0')
-		/* ASCII value for new line is 10*/
 		_putchar(10);
 	else
 	{
-		/* print first element of string*/
 		_putchar(s[0]);
-		/* move string and keep recursion going */
 		s++;
 		_puts_recursion(s);
 	}
